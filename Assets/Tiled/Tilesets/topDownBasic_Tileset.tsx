4518d7a3b04b1627244eccbe6b4e8ab1b4f1edc3
Trevor Baughn <63c73b5dd579d7c68754c51f@bots.bitbucket.org>
2023-02-09 19:29:21 -0700

    Updated Naming Conventions and Folders for Tiled

--- Assets/Tiled/Tilesets/topDownBasic_Tileset.tsx
+++ Assets/Tiled/Tilesets/topDownBasic_Tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="topDownBasic_Tileset" tilewidth="8" tileheight="8" tilecount="20" columns="4">
- <image source="topDownBasic_Tileset.png" width="32" height="40"/>
+ <image source="../../Art/Tilesets/topDownBasic_Tileset.png" width="32" height="40"/>
  <tile id="0">
   <properties>
    <property name="MatchType" value="NonEmpty"/>
