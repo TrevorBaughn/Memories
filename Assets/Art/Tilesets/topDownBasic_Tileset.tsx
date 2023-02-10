ce507f663e971362ed1f5713426a1bd2d0a51bb0
Trevor Baughn <63c73b5dd579d7c68754c51f@bots.bitbucket.org>
2023-02-09 19:22:24 -0700

    Added Tests for Automapping

---
+++ Assets/Art/Tilesets/topDownBasic_Tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="topDownBasic_Tileset" tilewidth="8" tileheight="8" tilecount="20" columns="4">
+ <image source="topDownBasic_Tileset.png" width="32" height="40"/>
+ <tile id="0">
+  <properties>
+   <property name="MatchType" value="NonEmpty"/>
+  </properties>
+ </tile>
+</tileset>
