882d4c4f819457e206d8649d31b9b90dbe9a65ce
Trevor Baughn <63c73b5dd579d7c68754c51f@bots.bitbucket.org>
2023-02-10 00:56:18 -0700

    Finished setting rules for tilemap

--- Assets/Tiled/Tilesets/topDownBasic_Tileset.tsx
+++ Assets/Tiled/Tilesets/topDownBasic_Tileset.tsx
@@ -1,20 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="topDownBasic_Tileset" tilewidth="8" tileheight="8" tilecount="20" columns="4">
- <image source="../../Art/Tilesets/topDownBasic_Tileset.png" width="32" height="40"/>
- <tile id="0">
-  <properties>
-   <property name="MatchType" value="NonEmpty"/>
-  </properties>
- </tile>
+<tileset version="1.9" tiledversion="1.9.2" name="topDownBasic_Tileset" tilewidth="8" tileheight="8" tilecount="25" columns="5">
+ <image source="../../Art/Tilesets/topDownBasic_Tileset.png" width="45" height="40"/>
  <tile id="3">
   <objectgroup draworder="index" id="3">
    <object id="3" x="0" y="0.03125" width="8" height="7.96875"/>
    <object id="4" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="4">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
  <tile id="5">
@@ -37,12 +27,12 @@
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="11">
+ <tile id="10">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="12">
+ <tile id="13">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
@@ -52,22 +42,27 @@
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="16">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
- <tile id="17">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="8"/>
-  </objectgroup>
- </tile>
  <tile id="18">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
- <tile id="19">
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
