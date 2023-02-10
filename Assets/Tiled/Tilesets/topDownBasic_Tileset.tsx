122c3da6e5a99b41d41128a8be731c9780967ded
Trevor Baughn <63c73b5dd579d7c68754c51f@bots.bitbucket.org>
2023-02-10 01:17:19 -0700

    Added floors

--- Assets/Tiled/Tilesets/topDownBasic_Tileset.tsx
+++ Assets/Tiled/Tilesets/topDownBasic_Tileset.tsx
@@ -1,10 +1,15 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="topDownBasic_Tileset" tilewidth="8" tileheight="8" tilecount="25" columns="5">
- <image source="../../Art/Tilesets/topDownBasic_Tileset.png" width="45" height="40"/>
+ <image source="../../Art/Tilesets/topDownBasic_Tileset.png" width="40" height="40"/>
  <tile id="3">
   <objectgroup draworder="index" id="3">
    <object id="3" x="0" y="0.03125" width="8" height="7.96875"/>
    <object id="4" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
   </objectgroup>
  </tile>
  <tile id="5">
