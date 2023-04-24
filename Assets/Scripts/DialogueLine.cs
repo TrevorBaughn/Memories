004bc9c2b46229deff246f38fe54ee2301c65f93
Trevor Baughn <63c73b5dd579d7c68754c51f@bots.bitbucket.org>
2023-04-24 15:18:50 -0700

    Lerp Dialogue

--- Assets/Scripts/DialogueLine.cs
+++ Assets/Scripts/DialogueLine.cs
@@ -6,13 +6,52 @@ using TMPro;
 public class DialogueLine : MonoBehaviour
 {
     TextMeshProUGUI text;
+    [SerializeField] GameObject pointOne;
+    [SerializeField] GameObject pointTwo;
 
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
 
-        StartCoroutine(Fade(text, 1.0f, true));
+        //StartCoroutine(Fade(text, 1.0f, true));
+        StartCoroutine(Lerp(this.gameObject, 2.0f, 0.25f, true));
+    }
+
+    public GameObject firstPoint;
+    public GameObject secondPoint;
+
+    public IEnumerator Lerp(GameObject textObject, float duration, float distanceLeeway, bool right)
+    {
+        text.horizontalAlignment = right ? HorizontalAlignmentOptions.Left : HorizontalAlignmentOptions.Right;
+
+        GameObject leftPoint = pointOne.transform.position.x < pointTwo.transform.position.x ? pointOne : pointTwo;
+        GameObject rightPoint = leftPoint == pointOne ? pointTwo : pointOne;
+
+        firstPoint = right ? leftPoint : rightPoint;
+        secondPoint = firstPoint == leftPoint ? rightPoint : leftPoint;
+
+        textObject.transform.position = firstPoint.transform.position;
+
+        float timeElapsed = 0;
+        while(timeElapsed < duration)
+        {
+            
+            textObject.transform.SetPositionAndRotation(Vector3.Lerp(firstPoint.transform.position,
+                                                                     secondPoint.transform.position,
+                                                                     timeElapsed / duration),
+                                                        Quaternion.identity);
+
+            timeElapsed += Time.deltaTime;
+
+            if(Vector3.Distance(textObject.transform.position, secondPoint.transform.position) < distanceLeeway)
+            {
+                yield break;
+            }
+
+            yield return null;
+        }
+
     }
 
     public IEnumerator Fade(TMP_Text tmtext, float speed, bool fadeIn)
